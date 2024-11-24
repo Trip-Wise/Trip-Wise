c93c51699a8f6a50436f2d4df844cbc73262318c
Trip-Wise <soulmax456@gmail.com>
2024-11-24 11:03:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -42,14 +42,14 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.redirect('signup.html');
+    res.redirect('/signup.html');
 });
 
 app.get('Trip.html', (req, res) => {
     if (!req.session.userName) {
-        return res.redirect('signup.html');
+        return res.redirect('/signup.html');
     }
-    res.redirect('Trip.html');
+    res.redirect('/Trip.html');
 });
 
 
@@ -71,7 +71,7 @@ app.get('/logout', (req, res) => {
         if (err) {
             return res.status(500).send('Error logging out.');
         }
-        res.redirect('login.html'); // Redirect to the login page after logout
+        res.redirect('/login.html'); // Redirect to the login page after logout
     });
 });
 
@@ -99,7 +99,7 @@ app.post('/signup', async (req, res) => {
         req.session.userId = user.id;
         req.session.userName = user.name;
 
-        res.redirect('Trip.html');
+        res.redirect('/Trip.html');
     } catch (err) {
         console.error('Error inserting user:', err);
         res.status(500).send('Error registering user');
